1a3e5ab9250b4e96aa265b49bc17836f16d5590e
Adam Binnersley <abinnersley@gmail.com>
2017-12-14 10:34:02 +0000

    Add basic unit tests

---
+++ tests/EventsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Calendar\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Calendar\Events;
+
+class EventsTest extends TestCase{
+    
+    public function setUp() {
+        parent::setUp();
+    }
+    
+    public function tearDown() {
+        parent::tearDown();
+    }
+    
+    public function testExample(){
+        $this->markTestIncomplete('This test has not yet been implemented');
+    }
+    
+}
---
+++ tests/ExportTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Calendar\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Calendar\Export;
+
+class ExportTest extends TestCase{
+    
+    public function setUp() {
+        parent::setUp();
+    }
+    
+    public function tearDown() {
+        parent::tearDown();
+    }
+    
+    public function testExample(){
+        $this->markTestIncomplete('This test has not yet been implemented');
+    }
+    
+}
